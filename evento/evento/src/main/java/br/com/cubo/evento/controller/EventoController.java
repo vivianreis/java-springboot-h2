062d8fbe4e79f2ef44195633c393ea8d9e893421
Vivian Barbosa Reis <srtvivian@hotmail.com>
2019-10-25 13:09:01 -0300

    api com métodos de put e delete usando spring boot + banco h2

--- evento/evento/src/main/java/br/com/cubo/evento/controller/EventoController.java
+++ evento/evento/src/main/java/br/com/cubo/evento/controller/EventoController.java
@@ -1,8 +1,11 @@
 package br.com.cubo.evento.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +29,25 @@ public class EventoController {
     public Evento save(@RequestBody Evento evento) {
     	return repository.save(evento);
     }
+    
+    @PutMapping(path = "{id}")
+    Evento replaceEvento(@RequestBody Evento newEvento, @PathVariable Integer id) {
+    	return repository.findById(id)
+    	.map(evento -> {
+    		evento.setNome(newEvento.getNome());
+    		evento.setLocal(newEvento.getLocal()); 
+    		return repository.save(evento);
+    	})
+    	.orElseGet(() -> {
+    		newEvento.setId(id);
+    		return repository.save(newEvento);
+    	});
+    }   
+    
+    @DeleteMapping(path = "{id}")
+    void deleteEvento(@PathVariable Integer id) {
+    	repository.deleteById(id);
+    }   
+          
+    
 }
--- evento/evento/src/main/java/br/com/cubo/evento/model/Evento.java
+++ evento/evento/src/main/java/br/com/cubo/evento/model/Evento.java
@@ -1,13 +1,35 @@
 package br.com.cubo.evento.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Evento {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String nome;
+	private String local;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
 
 	public Integer getId() {
 		return id;
